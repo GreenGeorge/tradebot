d0db8592992237d89204390c0fb96ae0780cb787
George Ananda <george@polymathco.com>
2017-10-14 16:25:57 +0700

    Setup dependencies Setup webpack Remove babel-cli Remove dist folder from repo Setup yarn scripts

---
+++ webpack.config.babel.js
@@ -0,0 +1,24 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'index.js',
+  },
+  resolve: {
+    extensions: ['.js'],
+  },
+  target: 'node',
+  module: {
+    rules: [
+      {
+        test: /\.js?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+    ],
+  },
+};
