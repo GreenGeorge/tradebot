ddf9ab8d5529598055b92fce2452203767606881
George Ananda <george@polymathco.com>
2017-10-15 04:11:57 +0700

    Add comments to clarify intent

--- src/index.js
+++ src/index.js
@@ -4,16 +4,21 @@ import colors from 'colors';
 import { getTicker } from './methods/requests';
 import { toIDR, toPercentage, toBTC } from './utils/numbers';
 
+// Create an object to hold the state
 const state = {
   balance: 0,
   boughtAt: 0,
   lastSoldAt: 0,
   lastPrice: 0,
 };
+// Set cap for money to trade
 const money = 100000;
+// Set intended return
 const margin = 0.05 / 100;
+// Set time between trades
 const interval = 2500;
 
+// Output a header
 console.log(`
 -----------------------------------------------
 Tradebot Started
@@ -24,45 +29,72 @@ Potential profit ${toIDR(money * margin)}
 -----------------------------------------------
 `.yellow);
 
+// Function to buy BTC
 const buyBTC = (amount, price) => {
+  // Get the amount of BTC to buy
   const BTCBought = amount / price;
+  // Add to the balance
   state.balance += BTCBought;
+  // Set the price / value of the balance
   state.boughtAt = price;
+  // Output the details of the transaction
   console.log(`bought ${toBTC(BTCBought)} at ${toIDR(price)}`);
 };
 
+// Function to sell BTC
 const sellBTC = (BTCamount, price) => {
+  // Reduce the balance
   state.balance -= BTCamount;
+  // Set the price / value of the balance sold
   state.lastSoldAt = price;
+  // Output the details of the transaction
   console.log(`sold ${toBTC(BTCamount)} at ${toIDR(price)}`);
 };
 
+// Function to check API and decide on action to achieve margin target
 const trade = async () => {
   try {
+    // Get the latest data from the API
     const data = await getTicker();
+    // Get the current price
     const currentPrice = parseInt(data.ticker.last, 10);
+    // Get the target price based on set margin
     const targetPrice = state.boughtAt * (1 + margin);
+    // Check if price has changed since last loop
     const priceChanged = currentPrice !== state.lastPrice;
+    // Check if no BTC is in the balance
     const outside = state.balance === 0;
+    // Check if the current price is above the set margin
     const aboveMargin = (currentPrice - targetPrice) > 0;
 
+    // If price has changed
     if (priceChanged) {
+      // Set the price in the state object
       state.lastPrice = currentPrice;
+      // Output the new price
       console.log(colors.red(toIDR(currentPrice)), state);
     }
 
+    // If no BTC is in the balance
     if (outside) {
+      // Buy BTC at current price
       buyBTC(money, currentPrice);
+      // Output the transaction details
       console.log(colors.red(toIDR(currentPrice)), state);
     }
 
+    // If BTC is in balance and current price is above the set margin
     if (!outside && aboveMargin) {
-      console.log(`Above margin! selling ${toBTC(state.balance)} at ${toIDR(currentPrice)}`);
+      console.log(`Above margin! selling ${toBTC(state.balance)} at
+       ${toIDR(currentPrice)}`);
+      // Sell BTC at current price
       sellBTC(state.balance, currentPrice);
     }
   } catch (error) {
+    // Output the error
     console.error(error);
   }
 };
 
+// Start program and loop
 setInterval(trade, interval);
--- src/methods/requests.js
+++ src/methods/requests.js
@@ -4,57 +4,77 @@ import { stringify } from 'query-string';
 
 import createSignature from './createSignature';
 
+// Get environment variables from .env file
 dotenv.config();
 
+// Set the API Key and Secret
 const BTCIDApiKey = process.env.BITCOINCOID_API_KEY;
 const BTCIDSecret = process.env.BITCOINCOID_SECRET;
 
+// Set the URLs for requests
 const domain = 'https://vip.bitcoin.co.id';
 const publicAPIBaseURL = `${domain}/api`;
 const privateAPIBaseURL = `${domain}/tapi`;
 
+// Curried function to do public requests
 const createPublicRequest = url => async () => {
   try {
+    // Initiate request to API
     const response = await axios({
       method: 'get',
       baseURL: publicAPIBaseURL,
       url,
     });
+    // If it's unsuccessful throw an error
     if (response.data.success === 0) throw new Error(response.data.error);
+    // Return the data
     return response.data;
   } catch (error) {
+    // Log the error
     console.error(error);
+    // Return blank object
     return {};
   }
 };
 
+// Curried function to do private requests with credentials
 const createPrivateRequest = method => async (params = {}) => {
   try {
+    // Create nonce from current time
     const nonce = (new Date()).getTime();
+    // Set the request parameters object
     const requestParams = {
       method,
       nonce,
       ...params,
     };
+    // Set the request body object
     const data = stringify(requestParams);
+    // Set the request headers object
     const headers = {
       Sign: createSignature(BTCIDSecret, data),
       Key: BTCIDApiKey,
     };
+    // Initiate request to API
     const response = await axios({
       method: 'post',
       baseURL: privateAPIBaseURL,
       data,
       headers,
     });
+    // If unsuccessful throw an error
     if (response.data.success === 0) throw new Error(response.data.error);
+    // Return the data
     return response.data;
   } catch (error) {
+    // Log the error
     console.error(error);
+    // Return a blank object
     return {};
   }
 };
 
+// Create the request functions
 export const getTicker = createPublicRequest('/btc_idr/ticker');
 export const getTrades = createPublicRequest('/btc_idr/trades');
 export const getDepth = createPublicRequest('/btc_idr/depth');
@@ -65,3 +85,4 @@ export const getOpenOrders = createPrivateRequest('openOrders');
 export const getOrderHistory = createPrivateRequest('orderHistory');
 export const getOrder = createPrivateRequest('getOrder');
 export const cancelOrder = createPrivateRequest('cancelOrder');
+export const trade = createPrivateRequest('trade');
